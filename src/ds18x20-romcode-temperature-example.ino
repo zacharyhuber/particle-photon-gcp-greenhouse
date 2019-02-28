96569cf025298e180a70e6533c385bac08c287a6
Zachary Huber <zachary.huber@gmail.com>
2019-02-28 11:24:53 -0600

    PRODUCT_VERSION(3)

--- src/ds18x20-romcode-temperature-example.ino
+++ src/ds18x20-romcode-temperature-example.ino
@@ -7,7 +7,7 @@
  */
 // Product ID and Version for Particle Product firmware deployment
 PRODUCT_ID(9008); // Argon version using DeviceOS v0.9.0
-PRODUCT_VERSION(2);
+PRODUCT_VERSION(3);
 
 // Semi-Automatic Mode allows collection of data without a network connection.
 // Particle.connect() will block the rest of the application code until a connection to Particle Cloud is established.
